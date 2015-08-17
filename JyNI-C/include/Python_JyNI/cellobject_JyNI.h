82b35193d9973f7d807e64beb17d801e2dfdc696
Stefan Richthofer <stefan.richthofer@gmx.de>
2015-08-17 23:17:37 +0200

    Changed license to LGPL.

--- JyNI-C/include/Python_JyNI/cellobject_JyNI.h
+++ JyNI-C/include/Python_JyNI/cellobject_JyNI.h
@@ -1,3 +1,30 @@
+/* This File is based on cellobject.h from CPython 2.7.10 release.
+ * It has been modified to suit JyNI needs.
+ *
+ * Copyright of the original file:
+ * Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+ * 2011, 2012, 2013, 2014, 2015 Python Software Foundation.  All rights reserved.
+ *
+ * Copyright of JyNI:
+ * Copyright (c) 2013, 2014, 2015 Stefan Richthofer.  All rights reserved.
+ *
+ *
+ * This file is part of JyNI.
+ *
+ * JyNI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * JyNI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with JyNI.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* Cell object interface */
 
 #ifndef Py_CELLOBJECT_H
@@ -20,9 +47,6 @@ PyAPI_FUNC(PyObject *) PyCell_Get(PyObject *);
 PyAPI_FUNC(int) PyCell_Set(PyObject *, PyObject *);
 
 #define PyCell_GET(op) (((PyCellObject *)(op))->ob_ref)
-/* JyNI-Note:
- * Modification via PyCell_SET is currently not sync'ed back to Jython properly!
- */
 #define PyCell_SET(op, v) (((PyCellObject *)(op))->ob_ref = v)
 
 #ifdef __cplusplus
--- JyNI-C/include/Python_JyNI/pyfpe_JyNI.h
+++ JyNI-C/include/Python_JyNI/pyfpe_JyNI.h
@@ -1,3 +1,30 @@
+/* This File is based on pyfpe.h from CPython 2.7.10 release.
+ * It has been modified to suit JyNI needs.
+ *
+ * Copyright of the original file:
+ * Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+ * 2011, 2012, 2013, 2014, 2015 Python Software Foundation.  All rights reserved.
+ *
+ * Copyright of JyNI:
+ * Copyright (c) 2013, 2014, 2015 Stefan Richthofer.  All rights reserved.
+ *
+ *
+ * This file is part of JyNI.
+ *
+ * JyNI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * JyNI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with JyNI.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef Py_PYFPE_H
 #define Py_PYFPE_H
 #ifdef __cplusplus
