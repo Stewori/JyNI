13db01c513fc98f4ee7ef6d1c6ade6f3a64d30aa
Stefan Richthofer <stefan.richthofer@gmx.de>
2016-03-06 23:34:34 +0100

    Restored JyNITkinterdemo.py; accidentally kept a temporarily out-commented minimal debug version in last commit.

--- JyNI-Demo/src/JyNITkinterTest.py
+++ JyNI-Demo/src/JyNITkinterTest.py
@@ -35,24 +35,24 @@ import sys
 sys.path.append('/usr/lib/python2.7/lib-dynload')
 sys.path.append('/usr/lib/python2.7/lib-tk')
 
-from Tkinter import Tk#, StringVar, Label, Entry, Button
+from Tkinter import Tk, StringVar, Label, Entry, Button
 
 root = Tk()
-# txt = StringVar()
-# txt.set("Hello World!")
-# 
-# def printText():
-# 	print txt.get()
-# 
-# def printTimeStamp():
-# 	from java.lang import System
-# 	print "System.currentTimeMillis: "+str(System.currentTimeMillis())
-# 
-# Label(root, text = "Welcome to JyNI Tkinter-Demo!").pack()
-# Entry(root, textvariable = txt).pack()
-# Button(root, text = "print text", command = printText).pack()
-# Button(root, text = "print timestamp", command = printTimeStamp).pack()
-# Button(root, text = "Quit", command = root.destroy).pack()
+txt = StringVar()
+txt.set("Hello World!")
+
+def printText():
+	print txt.get()
+
+def printTimeStamp():
+	from java.lang import System
+	print "System.currentTimeMillis: "+str(System.currentTimeMillis())
+
+Label(root, text = "Welcome to JyNI Tkinter-Demo!").pack()
+Entry(root, textvariable = txt).pack()
+Button(root, text = "print text", command = printText).pack()
+Button(root, text = "print timestamp", command = printTimeStamp).pack()
+Button(root, text = "Quit", command = root.destroy).pack()
 
 root.mainloop()
 
