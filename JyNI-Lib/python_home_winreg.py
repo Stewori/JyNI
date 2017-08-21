85d3446ffd8ebd05623657155befda969ef607a2
Stefan Richthofer <stefan.richthofer@gmx.de>
2017-08-21 20:35:26 +0200

    Let makefile.win get Python_Home from Windows registry via _winreg module.

---
+++ JyNI-Lib/python_home_winreg.py
@@ -0,0 +1,21 @@
+from _winreg import HKEY_LOCAL_MACHINE, HKEY_CURRENT_USER, QueryValue
+
+key1 = "SOFTWARE\\Python\\PythonCore\\2.7\\InstallPath"
+key2 = "SOFTWARE\\Wow6432Node\\Python\\PythonCore\\2.7\\InstallPath"
+
+def python_home():
+	try:
+		return QueryValue(HKEY_LOCAL_MACHINE, key2)
+	except WindowsError:
+		pass
+	try:
+		return QueryValue(HKEY_LOCAL_MACHINE, key1)
+	except WindowsError:
+		pass
+	try:
+		return QueryValue(HKEY_CURRENT_USER, key1)
+	except WindowsError:
+		pass
+
+if __name__ == '__main__':
+	print python_home()[:-1]
